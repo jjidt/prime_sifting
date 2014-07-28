945f1437ad43f5198479985f42e73290f48d8944
J.J. Idt <johnidt@gmail.com>
2014-07-27 20:08:26 -0700

    add functionality to prime_sifter method for checking single digit numbers

--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -5,4 +5,8 @@ describe "prime_sifter" do
   it "returns an array" do
     prime_sifter(2).should eq []
   end
+
+  it "takes a single digit number and returns the prime numbers less than that number" do
+    prime_sifter(9).should eq [2,3,5,7]
+  end
 end
